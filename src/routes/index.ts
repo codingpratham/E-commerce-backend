b17b943323fedd5a1757d416663cd870045e9a70
codingpratham <coding.job17@gmail.com>
2026-01-24 23:03:04 +0530

    all-code-done only api checking is done for the following code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import onboardingRoute from "./onboarding";
 import adminRoute from "./admin/admin-routes";
 import orderRoute from "./orders";
 import cartRoute from "./cart";
+import productRouter from "./products";
 const router = Router();
 
 router.use('/auth',authRoute)
@@ -11,6 +12,7 @@ router.use('/onboarding',onboardingRoute)
 router.use('/admin',adminRoute)
 router.use('/order',orderRoute)
 router.use('/cart',cartRoute)
+router.use('/product',productRouter)
 
 
 export default router;
